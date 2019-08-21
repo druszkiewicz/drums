bca973968e7e7a7081e2db66e73981ea1fe81e74
Dariusz Ruszkiewicz <dariusz.ruszkiewicz@bader-leather.com>
2019-08-21 15:04:36 +0200

    L. 140

--- index.js
+++ index.js
@@ -11,7 +11,13 @@
 var numberOfDrumButtons = document.querySelectorAll('.drum').length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
-   document.querySelectorAll('.drum')[i].addEventListener('click', function() {
-      alert('just clicked');
+   document.querySelectorAll('.drum')[i].addEventListener('click', function(e) {
+      //alert('just clicked');
+      console.log(this.innerHTML);
+      this.style.color = 'white';
+      console.log(e);
    });
 }
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
